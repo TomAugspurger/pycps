0c6d506c352c81ebcb16f548152c8f3b6e10c286
Tom Augspurger <tom.augspurger88@gmail.com>
2014-07-11 08:17:48 +0200

    remove name lookup in DD parser

--- pycps/parsers.py
+++ pycps/parsers.py
@@ -66,14 +66,18 @@ class DDParser:
     Parameters
     ----------
 
-    infile: A path to the data dictionary
-    outfile: path to HDFStore holding output.
-    style: Optional mmmYYYY for regex.
-    regex: Not used I think.
+    infile: pathlib.Path
+    settings: dict
+
+    Notes
+    -----
+
+    *file should be a Path object
+    *path should be a str.
     """
-    def __init__(self, infile, settings, style=None, regex=None):
+    def __init__(self, infile, settings):
         self.infile = infile
-        self.outfile = settings['dd_path']
+        self.outpath = settings['dd_path']
 
         if regex is None:
             self.regex = self.make_regex(style=style)
@@ -86,7 +90,7 @@ class DDParser:
         self.pos_len = 1
         self.pos_start = 2
         self.pos_end = 3
-        self.store_name = 
+        self.store_name = infile.stem
 
     def run(self):
         with open(self.infile, 'r') as f:
@@ -133,57 +137,6 @@ class DDParser:
             else:
                 self.holder.append(formatted)
 
-    def get_store_name(self):
-        """
-        #---------------------------------------------------------------------
-        NOTE: Usually, the documentation from January applies to an entire
-        year. Execptions are 1984-1985 and 1994-1995. The January 1984
-        documentation is used through to June 1985. The July 1985
-        documentation applies to the remainder of 1985. For 1994-1995,
-        the January 1994 documentation is used through August 1995.
-        The September 1995 documentation serves for the rest of the year.
-
-        #---------------------------------------------------------------------
-        Not sure about jan2003 going till april 2004.
-
-
-        We have {
-                jan1989: [1989-01, 1991-12], chk
-                jan1992: [1992-01, 1993-12], chk
-                jan1994: [1994-01, 1994-03], chk
-                apr1994: [1994-04, 1995-05], chk
-                jun1995: [1995-06, 1995-08], chk
-                sep1995: [1995-09, 1997-12], chk
-                jan1998: [1998-01, 2002-12], chk
-                jan2003: [2003-01, 2004-04], chk
-                may2004: [2004-05, 2005-07], chk
-                aug2005: [2005-08, 2006-12], chk
-                jan2007: [2007-01, 2008-12], chk
-                jan2009: [2009-01, 2009-12], chk
-                jan2010: [2010-01, 2012-04], boom #dat
-                may2012: [2012-05, 2012-12],
-                jan2013: [2013-01, 2013-03],
-                }
-        """
-        dict_ = {'cps89.ddf': 'jan1989',
-                 'cps92.ddf': 'jan1992',
-                 'cpsb_apr94_may95.ddf': 'apr1994',
-                 'cpsb_jan94_mar94.ddf': 'jan1994',
-                 'cpsb_jun95_aug95.ddf': 'jun1995',
-                 'cpsbsep95.ddf': 'sep1995',
-                 'cpsbjan98.ddf': 'jan1998',  # renamed this infile
-                 'cpsbjan03.ddf': 'jan2003',
-                 'cpsbmay04.ddf': 'may2004',
-                 'cpsbaug05.ddf': 'aug2005',
-                 'cpsbjan07.ddf': 'jan2007',
-                 'cpsbjan09.ddf': 'jan2009',
-                 'cpsbjan10.ddf': 'jan2010',
-                 'cpsbmay12.ddf': 'may2012',
-                 # Missing 2013. use alt parser
-                 }
-        inname = self.infile.split('/')[-1]
-        store_name = dict_[inname]
-        return store_name
 
     def handle_padding(self, formatted, f):
         """
@@ -275,7 +228,7 @@ class DDParser:
         Once you have all the dataframes, write them to that outfile,
         an HDFStore.
         """
-        store = pd.HDFStore(self.outfile)
+        store = pd.HDFStore(self.outpath)
         df = self.dataframes[0]  # Only should happen in the old ones.
         store.append(self.store_name, df, append=False)
         store.close()
