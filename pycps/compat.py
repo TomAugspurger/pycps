c01abc9e1a15636bbb1f351d7320fb3bc92f075b
Tom Augspurger <tom.augspurger88@gmail.com>
2014-07-12 00:00:16 +0200

    COMPAT: more compatability stuff

--- pycps/compat.py
+++ pycps/compat.py
@@ -7,5 +7,8 @@ ISPY2 = sys.version_info[0] == 2
 
 if ISPY2:
     from StringIO import StringIO
+    from functools import ifilter, map
+    filter = ifilter
+    map = map
 else:
     from io import StringIO
