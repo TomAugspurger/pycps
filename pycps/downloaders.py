09ea72fcaee8dac11257e9aaacbf40035bfaaa79
Tom Augspurger <tom.augspurger88@gmail.com>
2014-07-12 11:11:43 +0200

    ENH: add filter function

--- pycps/downloaders.py
+++ pycps/downloaders.py
@@ -10,27 +10,28 @@ TODO: March Supplements
 """
 import re
 import datetime
-from lxml import html
+from itertools import chain
 from functools import partial
+
+import arrow
+from lxml import html
+from pandas.core.common import is_list_like
 
 from pycps.parsers import read_settings
 
 
-def all_monthly_data():
+def all_monthly_files(site='http://www.nber.org/data/cps_basic.html'):
+    """
+    Find all matching monthly data files and data dictionaries
+    from the NBER's CPS site.
+    """
     regex = re.compile(r'cpsb\d{4}.Z|\w{3}\d{2}pub.zip|\.[ddf,asc]$')
-    site = 'http://www.nber.org/data/cps_basic.html'
     root = html.parse(site).getroot()
     partial_matcher = partial(_matcher, regex=regex)
 
-    for link in filter(partial_matcher, root.iterlinks()):
-        _, _, _fname, _ = link
-        fname = rename_cps_monthly(_fname.split('/')[-1])
-        existing = _exists(os.path.join(out_dir, fname))
-        if not existing:
-            downloader(fname, out_dir)
-            print('Added {}'.format(fname))
-
-    renamer(out_dir)
+    for _, _, fname_, _ in filter(partial_matcher, root.iterlinks()):
+        fname = fname_.split('/')[-1]
+        yield fname
 
 
 def rename_cps_monthly(cpsname):
@@ -51,13 +52,64 @@ def rename_cps_monthly(cpsname):
     if ext == 'Z':
         dt = datetime.datetime.strptime(fname, 'cpsb%y%m')
     else:
-        dt = datetime.datetime.strptime(fname, '%b%ypub.zip')
-    return dt.strftime('cpsm%Y-%m') + ext
+        dt = datetime.datetime.strptime(fname, '%b%ypub')
+    return dt.strftime('cpsm%Y-%m') + '.' + ext
 
 
+def filter_monthly_files(files, months=None, kind='both'):
+    """
+    Filter the generator from all_monthly_files down to
+    what you want, probably from the settings file.
 
-def monthly_data(month, **kwargs):
-    site = 'http://www.nber.org/data/cps_basic.html'
+    Parameters
+    ----------
+    filenames: str
+        in rename_cps_monthly style
+    months: list of, or list of list of, str or Arrow
+        Months to yield.
+        If list of list of str or Arrow, the lists will
+        be expanded to ranges and and values falling in
+        a range will be yielded (inclusive).
+        Should be 'YYYY-MM'.
+
+    kind: {'both', 'dd', 'data'}
+
+    Returns
+    -------
+    filtered: generator
+
+    Examples
+    --------
+
+    >>>filter_monthly_files(months=['1997-01', '1998-01', '1999-01'])
+
+    >>>filter_monthly_files(months=[['1994-01', '1994-06'],
+                                    ['1995-01', '1995-06']])
+    """
+    files = list(files)  # have to thunk
+    file_dates = [arrow.get(x.split('.')[0], format='cpsm%Y-%m')
+                  for x in files]
+
+    is_nested = [is_list_like(x) for x in months]
+    a = lambda x: arrow.get(x)
+
+    if any(is_nested) and not all(is_nested):
+        raise ValueError("Can't mix yet")
+    elif all(is_nested):
+        ranges = (arrow.Arrow.range('month', start=a(x), end=a(y))
+                  for (x, y) in months)
+        months = list(chain.from_iterable(ranges))
+    else:
+        months = [a(x) for x in months]
+
+    matched = filter(lambda x: x[1] in months, zip(files, file_dates))
+    for f, date in matched:
+        yield f
+
+
+def monthly_data(month,
+                 site='http://www.nber.org/data/cps_basic.html',
+                 **kwargs):
     root = html.parse(site).getroot()
 
 
