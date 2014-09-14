431e752ce44cbc2d92c67de020efdeb24b8d3036
TomAugspurger <thomas-augspurger@uiowa.edu>
2014-09-14 18:27:17 -0500

    LOG: add config and setup

--- pycps/__init__.py
+++ pycps/__init__.py
@@ -1,34 +1,24 @@
 # -*- coding: utf-8 -*-
+import os
+import json
 import logging
 import logging.config
 
 __version__ = '0.2.0'
 
+import pycps
 from pycps import api
 from pycps import compat
 from pycps import downloaders
 from pycps import parsers
 
+
 logger = logging.getLogger(__name__)
-logging.config.dictConfig({
-        'version': 1,
-        'disable_existing_loggers': True,
-        'formatters': {
-            'standard': {
-                    'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                },
-            },
-        'handlers': {
-            'default': {
-                'level': 'INFO',
-                'class': 'logging.StreamHandler'
-                },
-            },
-        'loggers': {
-            '': {
-                'handlers': ['default'],
-                'level': 'INFO',
-                'propagate': True
-                }
-            },
-    })
+
+def setup_logging(path_=None):
+    if path_ is None:
+        path_ = os.path.dirname(pycps.__file__) + '/logging.json'
+    with open (path_) as f:
+        config = json.load(f)
+    logging.config.dictConfig(config)
+
