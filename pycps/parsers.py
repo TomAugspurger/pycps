583268819e7bf152b207947bb4385667932ecf32
TomAugspurger <thomas-augspurger@uiowa.edu>
2014-09-23 11:00:18 -0500

    DATA: add cols and fixups for 1994-01 thru 1995-06

--- pycps/parsers.py
+++ pycps/parsers.py
@@ -552,10 +552,12 @@ def fixup_by_dd(df, dd_name):
         df['HRYEAR4'] = ('19' + df.HRYEAR4.astype(str)).astype(np.int64)
         return df
 
-    dispatch = {'cpsm1995-09': [compute_hrhhid2, year2_to_year4],
+    dispatch = {'cpsm1994-01': [compute_hrhhid2, year2_to_year4],
+                'cpsm1994-04': [compute_hrhhid2, year2_to_year4],
+                'cpsm1995-06': [compute_hrhhid2, year2_to_year4],
+                'cpsm1995-09': [compute_hrhhid2, year2_to_year4],
                 'cpsm1998-01': [compute_hrhhid2],
-                'cpsm2003-01': [compute_hrhhid2]
-               }
+                'cpsm2003-01': [compute_hrhhid2]}
 
     for func in dispatch.get(dd_name, []):
         df = func(df)
